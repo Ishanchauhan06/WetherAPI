0b011bab4e3f14894589a131b5b9276623d5c6c3
Ishan <chauhanishan82@gmail.com>
2022-08-02 19:28:01 +0530

    Full_project pushed

---
+++ weatherapp/weatherap/src/components/Tempapp.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import "./css/style.css";
+
+const Tempapp = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("Rishikesh");
+  const [iconi, setIcon] = useState(null);
+  const [desc, setDesc] = useState("");
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=b12f60798055697d4f33c0ef944f8e9e`;
+      // const imgurl =
+
+      const response = await fetch(url);
+      const resJson = await response.json();
+
+      setCity(resJson.main);
+      setIcon(resJson.weather[0]);
+      setDesc(resJson.weather[0]);
+    };
+    fetchApi();
+  }, [search]);
+  return (
+    <>
+      <div className="box">
+        <div className="inputData">
+          <input
+            type="search"
+            value={search}
+            className="inputField"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
+        {!city ? (
+          <p className="errorMsg">No Data Found</p>
+        ) : (
+          <div>
+            <div className="desim">
+              <img
+                src={`http://openweathermap.org/img/wn/${iconi.icon}@2x.png`}
+                className="imagedis"
+              ></img>
+              <p className="descimage">
+                {desc.description}
+              </p>
+            </div>
+            <div className="info">
+              <h2 className="location">
+                <i className="fas fa-street-view" ></i>
+                {search}
+              </h2>
+              <h1 className="temp">{city.temp}°Cel</h1>
+              <h3 className="tempmin_max">
+                Min: {city.temp_min}°Cel | Max:{city.temp_max}°Cel
+              </h3>
+            </div>
+            <div className="wave -one"></div>
+            <div className="wave -two"></div>
+            <div className="wave -three"></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Tempapp;
